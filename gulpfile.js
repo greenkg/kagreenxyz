a9e90e80cc0f0989cd88bb351da6ecc1a6df7e45
KA Green <greenkg@gmail.com>
2017-12-01 17:49:07 +0100

    Added Paris article

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ gulp.task("sassToCss", function() {
 gulp.task("watch", ["sassToCss", "build-all", "browserSync"], function() {
   gulp.watch("dev/styles/**/*.scss", ["sassToCss"]);
   gulp.watch("dev/**/*.html", ["build", browserSync.reload]);
-  gulp.watch("dev/pages/travel/*.html", ["build", browserSync.reload]);
+  gulp.watch("dev/pages/travel/*.html", ["build-all", browserSync.reload]);
   gulp.watch("dev/js/**/*.js", browserSync.reload);
 });
 
@@ -34,7 +34,7 @@ gulp.task('browserSync', function() {
   })
 });
 
-gulp.task("build-all", ["build", "build-iceland", "build-sedona", "build-msm"], function() {
+gulp.task("build-all", ["build", "build-iceland", "build-sedona", "build-msm", "build-jfj", "build-paris"], function() {
 
 });
 
@@ -57,4 +57,18 @@ gulp.task("build-msm", function() {
   .pipe(concat("index.html"))
     .on('error', sass.logError)
     .pipe(gulp.dest("build/travel/montstmichel"))
+});
+
+gulp.task("build-jfj", function() {
+  return gulp.src(["dev/pages/travel/partials/header.html", "dev/pages/travel/jungfraujoch.html", "dev/partials/footer.html"])
+  .pipe(concat("index.html"))
+    .on('error', sass.logError)
+    .pipe(gulp.dest("build/travel/jungfraujoch"))
+});
+
+gulp.task("build-paris", function() {
+  return gulp.src(["dev/pages/travel/partials/header.html", "dev/pages/travel/paris.html", "dev/partials/footer.html"])
+  .pipe(concat("index.html"))
+    .on('error', sass.logError)
+    .pipe(gulp.dest("build/travel/paris"))
 });
